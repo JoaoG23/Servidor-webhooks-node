03b40f43cfd8242fbd8ae860bf3055de63b11103
Joao_Guilherme2 <80895578+JoaoG23@users.noreply.github.com>
2025-02-27 11:54:16 -0300

    :sparkles: feat: code implementation

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Servidor-webhooks-node
 Webhooks são uma forma de permitir que um aplicativo envie informações em tempo real para outro aplicativo. Imagine que você tenha um sistema que precisa ser notificado sempre que algo acontece em outro sistema.
-![alt text](image.png)
+![alt text](image-1.png)
--- notification.js
+++ notification.js
@@ -7,9 +7,14 @@ app.use(express.json());
 app.post('/task-completed', (req, res) => {
   const taskId = req.body.taskId;
 
+  if (!taskId) {
+    res.status(400).send('Task ID is required.');
+    return;
+  }
   console.log(`Notification received: Task ${taskId} completed!`);
 
-  res.send('Notification received.');
+  // res.send('Notification received.');
+  res.send(`Task ${taskId} received notification!`);
 });
 
 app.listen(port, () => {
